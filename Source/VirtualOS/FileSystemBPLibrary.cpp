b19d709576b837fc7b707a911d6ec187dbf749ee
Anton Strandman <Marvin.Ztrubas@gmail.com>
2017-02-27 12:48:19 +0100

    Created file system c++ -> BP library

---
+++ Source/VirtualOS/FileSystemBPLibrary.cpp
@@ -0,0 +1,131 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VirtualOS.h"
+#include "FileSystemBPLibrary.h"
+
+TArray<FString> UFileSystemBPLibrary::GetFolderContent(FString path)
+{
+	class FFolderIterator : public IPlatformFile::FDirectoryVisitor
+	{
+	public:
+		TArray<FString> ResultPaths;
+		virtual bool Visit(const TCHAR* FilenameOrDirectory, bool bIsDirectory) {
+			ResultPaths.Add(FilenameOrDirectory);
+			return true;
+		}
+	};
+	FFolderIterator Iterator;
+
+	if (IsDirectory(*path))
+	{
+
+		IFileManager& FileManager = IFileManager::Get();
+		
+		FileManager.IterateDirectory(*path, Iterator);
+		
+	}
+	return Iterator.ResultPaths;
+}
+
+bool UFileSystemBPLibrary::IsDirectory(FString path)
+{
+	IFileManager& FileManager = IFileManager::Get();
+	if (FileManager.DirectoryExists(*path)) {
+		return FileManager.GetStatData(*path).bIsDirectory;
+	}
+	else return false;
+	
+	
+}
+
+bool UFileSystemBPLibrary::IsValid(FString path)
+{
+	return IFileManager::Get().GetStatData(*path).bIsValid;
+
+}
+
+int32 UFileSystemBPLibrary::FileSize(FString path)
+{
+	IFileManager& Manager = IFileManager::Get();
+	if (Manager.GetStatData(*path).bIsValid) {
+		return Manager.GetStatData(*path).FileSize;
+	}
+		return -1;
+}
+
+bool UFileSystemBPLibrary::MakeDirectory(FString pathToCreate, bool makeNeededSubDir)
+{
+	IFileManager& Manager = IFileManager::Get();
+	if (!Manager.DirectoryExists(*pathToCreate)) return Manager.MakeDirectory(*pathToCreate, makeNeededSubDir);
+	return false;
+}
+
+bool UFileSystemBPLibrary::MoveFile(FString source, FString target, bool replaceIfExists)
+{
+	IFileManager& Manager = IFileManager::Get();
+	if (Manager.FileExists(*source)) {
+		return Manager.Move(*target, *source, replaceIfExists);
+	}
+	return false;
+}
+
+bool UFileSystemBPLibrary::CopyFile(FString sourceFile, FString targetFilePathName, bool replaceIfExists)
+{
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	if (PlatformFile.FileExists(*sourceFile)) {
+		return PlatformFile.CopyFile(*targetFilePathName, *sourceFile);
+	}
+	return false;
+}
+
+bool UFileSystemBPLibrary::MoveFolder(FString rootToMove, FString destinationDir, bool overwriteExisting)
+{
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	if (PlatformFile.DirectoryExists(*rootToMove) && PlatformFile.DirectoryExists(*destinationDir) &&
+		PlatformFile.CopyDirectoryTree(*destinationDir, *rootToMove, overwriteExisting)) {
+		PlatformFile.DeleteDirectoryRecursively(*rootToMove);
+	}
+	return false;
+}
+
+bool UFileSystemBPLibrary::CopyFolder(FString rootToCopy, FString destinationDir, bool overwriteExisting)
+{
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	if (PlatformFile.DirectoryExists(*rootToCopy) && PlatformFile.DirectoryExists(*destinationDir)) {
+		return PlatformFile.CopyDirectoryTree(*destinationDir, *rootToCopy, overwriteExisting);
+	}
+	else return false;
+	
+}
+
+bool UFileSystemBPLibrary::RenameFile(FString source, FString newName)
+{	
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	if (PlatformFile.FileExists(*source)) {
+		int indexAt;
+		if (source.FindLastChar('/', indexAt)) {
+			FString rootOfFile = source.Left(indexAt);
+			rootOfFile.Append(newName);
+			return PlatformFile.MoveFile(*newName, *source);
+		}
+	}
+	return false;
+}
+
+bool UFileSystemBPLibrary::DeleteFile(FString path)
+{
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	if (PlatformFile.FileExists(*path)) {
+		return PlatformFile.DeleteFile(*path);
+	}
+	return false;
+}
+
+bool UFileSystemBPLibrary::DeleteFolder(FString path)
+{
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	if (PlatformFile.DirectoryExists(*path)) {
+		return PlatformFile.DeleteDirectory(*path);
+	}
+	return false;
+}
