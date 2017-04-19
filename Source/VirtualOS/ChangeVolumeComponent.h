7893802a50db43a4fc8e82e37f0c9cb577b31c53
Anton <anton.solback@gmail.com>
2017-04-19 09:16:44 +0200

    Fixed so now 'Master volume' changes windows sound directly and the other sounds are default 0. Also maybe fixed error in skybox audio

---
+++ Source/VirtualOS/ChangeVolumeComponent.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "ChangeVolumeComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class VIRTUALOS_API UChangeVolumeComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UChangeVolumeComponent();
+
+	//Custom function to change the sound of Windows
+	UFUNCTION(BlueprintCallable, Category = "Custom functions")
+		void changeMasterVolume(float newSoundLevel);
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+		
+	
+};
